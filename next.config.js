617542c400a405a1e92adfa3b863e40ba736e4cb
Mark Ursino <mursino@gmail.com>
2023-10-13 11:19:57 -0400

    remove redirect api and rewrite

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/_redirects",
-        destination: "/api/redirects",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
